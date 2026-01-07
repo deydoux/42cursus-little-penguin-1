02cbb39a2f9866de01820cf05a06876121ec158b
Dorian Eydoux <deydoux@student.42lyon.fr>
2026-01-07 15:12:30 +0100

    Add read.c: implement a simple program to read from the character device /dev/fortytwo

---
+++ ex05/test/read.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+	int fd = open("/dev/fortytwo", O_RDONLY);
+
+	char c;
+	ssize_t bytes_read = read(fd, &c, 1);
+	while (bytes_read) {
+		printf("bytes_read: %zd, c: %d '%c'\n", bytes_read, c, c);
+		bytes_read = read(fd, &c, 1);
+	}
+
+	close(fd);
+}
