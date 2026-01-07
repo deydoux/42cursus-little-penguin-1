48f295cecb8f8779518de2ec77e32a5bf2e4c96b
Dorian Eydoux <deydoux@student.42lyon.fr>
2026-01-07 15:34:03 +0100

    Refactor ft_dev module: remove clean function and related state management, simplify initialization with miscdevice

--- ex05/include/ft_dev.h
+++ ex05/include/ft_dev.h
@@ -1,7 +1,6 @@
 #ifndef __FT_DEV_H__
 # define __FT_DEV_H__
 
-#include <linux/cdev.h>
 #include <linux/module.h>
 
 # ifndef FT_LOGIN
@@ -12,26 +11,10 @@
 # define FT_LOGIN_LEN ((sizeof FT_LOGIN) - 1)
 # define PRINT_PREFIX "ft_dev: "
 
-typedef enum ft_dev_state_e
-{
-	FT_DEV_INIT,
-	FT_DEV_ALLOC_CHRDEV_REGION,
-	FT_DEV_CDEV_INIT,
-	FT_DEV_CDEV_ADD,
-	FT_DEV_CLASS_CREATE,
-	FT_DEV_DEVICE_CREATE
-}	ft_dev_state_t;
-
-void ft_dev_clean(ft_dev_state_t state);
 int ft_dev_open(struct inode *inode, struct file *file);
 ssize_t ft_dev_read(struct file *filp, char *buf, size_t len, loff_t *off);
 int ft_dev_release(struct inode *inode, struct file *file);
 ssize_t ft_dev_write(struct file *filp, const char *buf, size_t len,
 	loff_t *off);
-
-extern dev_t dev;
-extern struct cdev cdev;
-extern struct class *class;
-extern ft_dev_state_t state;
 
 #endif /* __FT_DEV_H__ */
--- ex05/src/ft_dev.c
+++ ex05/src/ft_dev.c
@@ -1,60 +1,29 @@
+#include <linux/miscdevice.h>
 #include "ft_dev.h"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("An Hello World kernel module");
 
-dev_t dev;
-struct cdev cdev;
-struct class *class;
-ft_dev_state_t state = FT_DEV_INIT;
+static const struct file_operations fops = {
+	.open = ft_dev_open,
+	.release = ft_dev_release,
+	.read = ft_dev_read,
+	.write = ft_dev_write,
+};
+
+static struct miscdevice misc = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = DEV_NAME,
+	.fops = &fops
+};
 
 static int __init ft_dev_init(void)
 {
-	int ret;
-
-	ret = alloc_chrdev_region(&dev, 0, 1, DEV_NAME);
-	if (ret < 0) {
-		printk(KERN_ALERT PRINT_PREFIX \
-			"Failed to allocate char device region\n");
-		ft_dev_clean(state);
+	int ret = misc_register(&misc);
+	if (ret) {
+		printk(KERN_ALERT PRINT_PREFIX "Failed to register misc device\n");
 		return ret;
 	}
-	state = FT_DEV_DEVICE_CREATE;
-
-	static struct file_operations fops = {
-		.open = ft_dev_open,
-		.release = ft_dev_release,
-		.read = ft_dev_read,
-		.write = ft_dev_write
-	};
-
-	cdev_init(&cdev, &fops);
-	state = FT_DEV_CDEV_INIT;
-
-	ret = cdev_add(&cdev, dev, 1);
-	if (ret < 0) {
-		printk(KERN_ALERT PRINT_PREFIX "Failed to add cdev\n");
-		ft_dev_clean(state);
-		return ret;
-	}
-	state = FT_DEV_CDEV_ADD;
-
-	class = class_create(DEV_NAME);
-	if (IS_ERR(class)) {
-		printk(KERN_ALERT PRINT_PREFIX "Failed to create class\n");
-		ft_dev_clean(state);
-		return PTR_ERR(class);
-	}
-	state = FT_DEV_CLASS_CREATE;
-
-	struct device *dev_node;
-	dev_node = device_create(class, NULL, dev, NULL, DEV_NAME);
-	if (IS_ERR(dev_node)) {
-		printk(KERN_ALERT PRINT_PREFIX "Failed to create device\n");
-		ft_dev_clean(state);
-		return PTR_ERR(dev_node);
-	}
-	state = FT_DEV_DEVICE_CREATE;
 
 	printk(KERN_INFO PRINT_PREFIX "Registered\n");
 	return 0;
@@ -62,7 +31,7 @@ static int __init ft_dev_init(void)
 
 static void __exit ft_dev_exit(void)
 {
-	ft_dev_clean(state);
+	misc_deregister(&misc);
 	printk(KERN_INFO PRINT_PREFIX "Unregistered\n");
 }
 
