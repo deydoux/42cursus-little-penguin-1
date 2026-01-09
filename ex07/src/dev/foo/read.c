40b52c0ecc12c23b3c918669b41e8e8b0ef5c0d7
Dorian Eydoux <deydoux@student.42lyon.fr>
2026-01-09 16:15:08 +0100

    Fix logging format in dev_foo_read function to use correct size type

--- ex07/src/dev/foo/read.c
+++ ex07/src/dev/foo/read.c
@@ -5,7 +5,8 @@ ssize_t dev_foo_read(struct file *filp, char *buf, size_t len, loff_t *off)
 {
 	// ssize_t bytes = min(FT_LOGIN_LEN - *off, len);
 
-	pr_info(PR_PREFIX "Read %zd with offset %lld\n", bytes, *off);
+	pr_info(PR_PREFIX "Read %zu with offset %lld\n", len, *off);
+	// pr_info(PR_PREFIX "Read %zd with offset %lld\n", bytes, *off);
 	// if (bytes <= 0)
 	// 	return 0;
 
