71559dcd043d75b4831ec75cd2b6411e4151b9e8
Dorian Eydoux <deydoux@student.42lyon.fr>
2026-01-09 16:35:55 +0100

    Add mutex for dev_foo to ensure thread safety during operations

--- ex07/src/dev/foo/init.c
+++ ex07/src/dev/foo/init.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0+
 #include "dev/foo.h"
 
+struct mutex dev_foo_lock;
+
 static const struct file_operations fops = {
 	.open = dev_foo_open,
 	.release = dev_foo_release,
@@ -12,6 +14,8 @@ int dev_foo_init(struct dentry *parent)
 {
 	struct dentry *entry = debugfs_create_file(DEV_NAME, DEV_MODE, parent,
 		NULL, &fops);
+
+	mutex_init(&dev_foo_lock);
 
 	if (!entry) {
 		pr_alert(PR_PREFIX "Failed to create device\n");
